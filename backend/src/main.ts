ed3dad64d9a1f387de6b00e350eb3159817b1602
amirtalbi <amirotalbi94@gmail.com>
2024-11-25 11:52:05 +0100

    KAN-19 feat: adding host from env file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const host = process.env.HOST ?? '0.0.0.0';
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, host);
 }
 bootstrap();
