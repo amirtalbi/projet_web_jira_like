3b1f122ec7df1adb66924aeebf85c61e99a92d38
amirtalbi <amirotalbi94@gmail.com>
2024-11-26 10:48:11 +0100

    KAN-19 feat: adding backend architecture Co-authored-by: Farid <farid.ousmane01@gmail.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TaskModule } from './task/task.module';
+import { SubtaskModule } from './subtask/subtask.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ProjectsModule, TaskModule, SubtaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
