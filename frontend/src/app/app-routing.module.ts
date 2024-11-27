b9efb492b2fb2141449d4128889d4547b433dbac
jibe <jibeharmoniq@gmail.com>
2024-11-27 12:20:24 +0100

    KAN18: deletion of useless file and code

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
-import { AuthComponent } from './auth/login/auth.component';
 
 const routes: Routes = [
   { path: 'auth/login', component: AuthComponent },
