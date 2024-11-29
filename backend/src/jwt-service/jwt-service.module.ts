cbb19eaa3b81c5ea4a3d6b5b61e893e68f0b5648
amirtalbi <amirotalbi94@gmail.com>
2024-11-29 10:47:07 +0100

    fix: adding global config module

--- backend/src/jwt-service/jwt-service.module.ts
+++ backend/src/jwt-service/jwt-service.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from './jwt-service.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   providers: [JwtService],
   exports: [JwtService],
 })
